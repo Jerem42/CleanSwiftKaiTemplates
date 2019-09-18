206a9285e3bf0459a08773ff60866ae7c9f47cc0
Jérémy Bastien <jeremy.bastien42@gmail.com>
2019-09-18 15:12:14 +0200

    Added StoryboardInstantiable protocol

---
+++ StoryboardInstantiable.swift
@@ -0,0 +1,20 @@
+//___FILEHEADER___
+
+import Foundation
+import UIKit
+
+protocol StoryboardInstantiable {
+    static var storyboardName: String { get }
+}
+
+extension StoryboardInstantiable where Self: UIViewController {
+    
+    static var storyboardInstance: Self? {
+        guard let storyboardId = String(describing: self).components(separatedBy: (".")).last else {
+            return nil
+        }
+        let storyboard = UIStoryboard(name: storyboardName, bundle: Bundle.mmtBundle)
+        let viewController = storyboard.instantiateViewController(withIdentifier: storyboardId)
+        return viewController as? Self
+    }
+}
